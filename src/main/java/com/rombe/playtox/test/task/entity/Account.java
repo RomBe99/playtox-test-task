229c8509dfcbdcdcfc022943945f224a8f749c73
Belinsky Roman <romanbelinsky99@gmail.com>
2021-12-14 20:09:47 +0600

    Add data class Account

---
+++ src/main/java/com/rombe/playtox/test/task/entity/Account.java
@@ -0,0 +1,49 @@
+package com.rombe.playtox.test.task.entity;
+
+import java.util.Objects;
+
+public class Account {
+    private String id = "";
+    private int money = 0;
+
+    public Account() {
+    }
+
+    public Account(String id) {
+        this.id = id;
+    }
+
+    public Account(String id, int money) {
+        this.id = id;
+        this.money = money;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return money == account.money && Objects.equals(id, account.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, money);
+    }
+}
