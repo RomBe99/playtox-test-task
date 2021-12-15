774fe98a68592e8808f2939dc527441ab28b41b4
Belinsky Roman <romanbelinsky99@gmail.com>
2021-12-15 15:14:27 +0600

    Add class AccountTransactionManager for managing transactions

---
+++ src/main/java/com/rombe/playtox/test/task/transaction/account/AccountTransactionManager.java
@@ -0,0 +1,60 @@
+package com.rombe.playtox.test.task.transaction.account;
+
+import com.rombe.playtox.test.task.db.account.AccountDatabase;
+import com.rombe.playtox.test.task.entity.Account;
+import com.rombe.playtox.test.task.transaction.Transaction;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AccountTransactionManager {
+    private final AtomicInteger transactionsCount;
+    private final BlockingQueue<Transaction> transactions = new LinkedBlockingQueue<>();
+    private final AccountDatabase accountDatabase;
+
+    public AccountTransactionManager(AccountDatabase accountDatabase, int transactionAmount) {
+        this.accountDatabase = accountDatabase;
+        this.transactionsCount = new AtomicInteger(transactionAmount);
+    }
+
+    public void beginNextTransaction() {
+        try {
+            final var transaction = transactions.poll();
+
+            if (transaction != null) {
+                transaction.execute();
+                transaction.commit();
+            }
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean registerMoneyTransaction(String senderId, int amountMoneyToSend, String recipientId) {
+        return registerNewTransaction(new MoneyTransaction(accountDatabase, senderId, amountMoneyToSend, recipientId));
+    }
+
+    public boolean registerCreateNewAccountTransaction(Account newAccount) {
+        return registerNewTransaction(new CreateNewAccountTransaction(accountDatabase, newAccount));
+    }
+
+    public boolean hasTransactions() {
+        return !transactions.isEmpty();
+    }
+
+    public boolean hasFreeTransactions() {
+        return transactionsCount.get() > 0;
+    }
+
+    private boolean registerNewTransaction(Transaction transaction) {
+        if (!hasFreeTransactions()) {
+            return false;
+        }
+
+        transactions.add(transaction);
+        transactionsCount.decrementAndGet();
+
+        return true;
+    }
+}
